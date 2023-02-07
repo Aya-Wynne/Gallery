133a176f2ec9b3493a2c1b4fea379bd53f6b47bc
Aya-Wynne <ayabolt@hotmail.com>
2023-02-07 22:26:33 +0000

    First button

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function readMore() {
-    console.log('here')
-    var showText = document.getElementById ("para1");
-    var myBtn = document.getElementById ("btn");
-    console.log(showText.style.display)
-    if (showText.style.display === 'none') {
-        myBtn.innerHTML = "Read More";
-    } else {
-        showText.style.display = "inline";
-        myBtn.innerHTML = "Read Less";
+    var info1 = document.getElementById("para1");
+    var myBtn = document.getElementById("btn");
+    info1.classList.toggle("hidden")
+    info1.classList.toggle("show")
+
+    if (info1.classList.contains("hidden")){
+        myBtn.innerText = "Read More";
+    }
+    else { 
+    myBtn.innerText= "Read Less";
     }
 }
-  
